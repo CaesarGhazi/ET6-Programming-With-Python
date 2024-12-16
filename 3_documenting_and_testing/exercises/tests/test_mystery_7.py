ba2f6a54d4e8c40304d43e4ce14c8a7421d2051f
CaesarGhazi <caesarghazi13@gmail.com>
2024-12-16 23:22:40 +0200

    edits

--- 3_documenting_and_testing/exercises/tests/test_mystery_7.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_7.py
@@ -4,8 +4,13 @@ from ..mystery_7 import mystery_7
 
 class TestMystery7(unittest.TestCase):
     """ """
-    def test_random_input(self):
-        """It should return the list in order"""
-        actual = mystery_7() 
-        expected = [0, 1, 2, 3, 4]
+    def test_numbers_in_list(self):
+        """Test with numbers in the list"""
+        actual = mystery_7([[1, 2, 3], [4, 5, 6], [7, 8, 1]], 1) 
+        expected = [[1, 2, 3], [7, 8, 1]]
+        self.assertEqual(actual, expected)
+    def test_empty_list(self):
+        """Test with empty list"""
+        actual = mystery_7([], []) 
+        expected = []
         self.assertEqual(actual, expected)
